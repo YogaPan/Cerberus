6cb97c1e37dfb5d0ad46141b89971f297d1fc0bb
HA47 <hank84112092@gmail.com>
2017-05-10 01:05:34 +0800

    conflict

--- components/Chat.vue
+++ components/Chat.vue
@@ -14,7 +14,7 @@
             <div class="message">
               {{ message.content }}
             </div>
-    
+
             <div class="msg-status">
               <div class="msg-status-read" v-if="message.read">
                 已讀
@@ -47,7 +47,6 @@
 
 <script>
 import { mapGetters } from 'vuex'
-
 export default {
   data() {
     return {
@@ -59,11 +58,7 @@ export default {
       messages: 'messages'
     }),
     typing() {
-      if (this.input === '') {
-        return false
-      } else {
-        return true
-      }
+      return (this.input === '') ? true : false
     }
   },
   methods: {
@@ -74,7 +69,6 @@ export default {
       if (this.input !== '') {
         this.$store.dispatch('submit', this.input)
         this.input = ''
-
         const messageContainer = this.$el.querySelector(".message-container")
         messageContainer.scrollTop = messageContainer.scrollHeight
       }
@@ -84,31 +78,24 @@ export default {
 </script>
 
 <style scoped>
-
 #app-chat {
   position: absolute;
   top: 0;
   right: 0;
-
   height: 100vh;
   width: 350px;
-
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: stretch;
-
   border-left: 2px solid black;
 }
-
 .message-container {
   overflow: scroll;
   overflow-x: hidden;
-
   height: 100vh;
   width: 100%;
   align-self: stretch;
-
   padding: 10px 20px;
 }
 .single-message {
@@ -158,49 +145,40 @@ export default {
 .msg-status-read {
   line-height: 12px;
 }
-
 .msg-status-time {
   line-height: 12px;
 }
-
 .is-typing {
   display: flex;
   align-items: center;
   justify-content: center;
-
   height: 40px;
   align-self: stretch;
 }
-
 .user-input {
   height: 35px;
 }
-
 .user-input input {
   /* box model */
   -webkit-box-sizing: border-box;
      -moz-box-sizing: border-box;
           box-sizing: border-box;
-
   /* border and shape */
   height: 35px;
   width: 100%;
   border: 2px solid #ddd;
   border-radius: 5px;
-
   /* font and padding */
   padding-left: 8px;
   font-size: 15px;
   color: #444;
   font-family: sans-serif;
 }
-
 /* when user click this input */
 .user-input input:focus {
   outline: none;
   border: 2px solid #aaa;
 }
-
 p {
   color: ;
   margin: 0px;
