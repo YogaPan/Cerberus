910bc99cc83fc586db8342876d22483c8cdc141d
YogaPan <yogapan85321@gmail.com>
2017-06-03 17:11:02 +0800

    獲取視訊鏡頭

--- components/Camera.vue
+++ components/Camera.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 id="title">Camera Area</h1>
+  <div id="camera-container">
+    <video id="camera" autoplay></video>
+  </div>
 </template>
 
 <script>
@@ -9,13 +11,38 @@ export default {
     return {
       /* TODO */
     }
+  },
+  mounted() {
+    if (this.hasGetUserMedia()) {
+      navigator.getUserMedia({ video: true, audio: true }, stream => {
+        const video = this.$el.querySelector('#camera')
+        video.src = window.URL.createObjectURL(stream)
+        video.muted = true; // no sound feedback
+      }, error => {
+        alert('ERROR!')
+      })
+    } else {
+      alert('getUserMedia() is not supported in your browser')
+    }
+  },
+  methods: {
+    hasGetUserMedia() {
+      return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia)
+    }
   }
 }
 
 </script>
 
 <style>
-#title {
-  color: #aaa;
+#camera-container {
+  width: 100%;
+  height: 100%;
+}
+
+#camera {
+  /*object-fit: fill;*/
+  width: 100%;
 }
 </style>
