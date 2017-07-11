9f491dba0158fbae2d0b0fe985b267093b3d20d9
YogaPan <yogapan85321@gmail.com>
2017-07-11 17:37:21 +0800

    test

--- app/chatroom/router/Whiteboard.vue
+++ app/chatroom/router/Whiteboard.vue
@@ -30,120 +30,14 @@ export default {
   },
   mounted() {
     this.canvas = this.$el.querySelector('#whiteboard')
-    this.colors =this.$el.querySelector('.color')
     this.context = this.canvas.getContext('2d');
 
-    // var current = {
-      // color: 'black'
-    // };
-    // var drawing = false;
-
-    /*
-    canvas.addEventListener('mousedown', onMouseDown, false);
-    canvas.addEventListener('mouseup', onMouseUp, false);
-    canvas.addEventListener('mouseout', onMouseUp, false);
-    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
-    */
-
-    /*
-    for (var i = 0; i < colors.length; i++){
-      colors[i].addEventListener('click', onColorUpdate, false);
-    }
-    */
+    this.colors =this.$el.querySelector('.color')
 
     socket.on('drawing', onDrawingEvent);
 
-    // window.addEventListener('resize', onResize, false);
-    // onResize();
-
-    /*
-    function drawLine(x0, y0, x1, y1, color, emit){
-      context.beginPath();
-      context.moveTo(x0, y0);
-      context.lineTo(x1, y1);
-      context.strokeStyle = color;
-      context.lineWidth = 2;
-      context.stroke();
-      context.closePath();
-
-      if (!emit) { return; }
-      var w = canvas.width;
-      var h = canvas.height;
-
-      socket.emit('drawing', {
-        x0: x0 / w,
-        y0: y0 / h,
-        x1: x1 / w,
-        y1: y1 / h,
-        color: color
-      });
-    }
-    */
-
-    /*
-    function onMouseDown(e){
-      this.drawing = true;
-      this.current.x = e.clientX;
-      this.current.y = e.clientY;
-
-      // DEBUG
-      console.log(this.current)
-    }
-    */
-
-    /*
-    function onMouseUp(e){
-      if (!this.drawing) { return; }
-      this.drawing = false;
-      drawLine(current.x, current.y, e.clientX, e.clientY, this.current.color, true);
-    }
-    */
-
-    /*
-    function onMouseMove(e){
-      if (!this.drawing) { return; }
-      drawLine(this.current.x, this.current.y, e.clientX, e.clientY, this.current.color, true);
-      this.current.x = e.clientX;
-      this.current.y = e.clientY;
-    }
-    */
-
-    /*
-    function onColorUpdate(e){
-      current.color = e.target.className.split(' ')[1];
-    }
-    */
-
-    /*
-    // limit the number of events per second
-    function throttle(callback, delay) {
-      var previousCall = new Date().getTime();
-      return function() {
-        var time = new Date().getTime();
-
-        if ((time - previousCall) >= delay) {
-          previousCall = time;
-          callback.apply(null, arguments);
-        }
-      };
-    }
-    */
-
-    /*
-    function onDrawingEvent(data){
-      var w = canvas.width;
-      var h = canvas.height;
-      drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-    }
-    */
-
-    /*
-    // make the canvas fill its parent
-    function onResize() {
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    }
-    */
+    window.addEventListener('resize', this.onResize, false);
+    onResize();
   },
   methods: {
     changeColor(color) {
@@ -186,13 +80,17 @@ export default {
     onMouseUp(e) {
       if (!this.drawing) { return }
       this.drawing = false
-      this.drawLine(current.x, current.y, e.clientX, e.clientY, this.current.color, true)
+      this.drawLine(this.current.x, this.current.y, e.clientX, e.clientY, this.current.color, false)
     },
     onMouseMove(e) {
-      if (!this.drawing) { return; }
-      this.drawLine(this.current.x, this.current.y, e.clientX, e.clientY, this.current.color, true)
+      if (!this.drawing) { return }
+      this.drawLine(this.current.x, this.current.y, e.clientX, e.clientY, this.current.color, false)
       this.current.x = e.clientX
       this.current.y = e.clientY
+    },
+    onResize() {
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight;
     },
     throttle(callback, delay) {
       var previousCall = new Date().getTime();
@@ -213,25 +111,16 @@ export default {
 #whiteboard-container {
   flex: 1 0 0;
 
-  display : flex;
-  flex-direction: column;
-  align-items: stretch;
-  justify-content: center;
+  /*display : flex;*/
+  /*flex-direction: column;*/
+  /*align-items: stretch;*/
+  /*justify-content: center;*/
 
   background-color: #fff;
 }
 
 #whiteboard {
-  flex: 1 0 0;
-  /*
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  */
+  /*flex: 1 0 0;*/
 }
 
 .colors {
