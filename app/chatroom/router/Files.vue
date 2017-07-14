4bb628c8388adb75586086b18f9056437abc6017
YogaPan <yogapan85321@gmail.com>
2017-07-14 15:01:32 +0800

    Add file uploader

--- app/chatroom/router/Files.vue
+++ app/chatroom/router/Files.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="files-container">
     <h1>Files here...</h1>
+    <form action="/test" method="post" enctype="multipart/form-data">
+      <input type="file" accept="" name="filetoupload">
+      <input type="submit">
+    </form>
   </div>
 </template>
 
