c8984646089ba0f91f7e929cf0269d487b8201d7
YogaPan <yogapan85321@gmail.com>
2017-08-17 15:59:42 +0800

    投票發問系統

--- app/chatroom/router/Vote.vue
+++ app/chatroom/router/Vote.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="vote-container">
+  <div id="question-container">
 
     <div class="inside-container">
-      <h1>Vote Title</h1>
-      <input class="input-big" type="text" />
 
-      <h1>Options</h1>
-      <input class="input" type="text" />
+      <div class="question-title">
+        <!-- <p>Vote Title</p> -->
+        <input class="input-big" type="text" placeholder="Question Title" />
+      </div>
 
-      <h1>Options</h1>
-      <input class="input" type="text" />
+      <div class="question" v-for="question in questions">
+        <p>Question {{ question.id }}</p>
+        <input class="input" type="text" />
+      </div>
+
+      <div class="new-question" @click="addNewQuestion">
+
+      </div>
+
+      <button id="question-button" class="button-trans">Emit vote event</button>
+
     </div>
   </div>
 </template>
@@ -17,19 +26,29 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      questions: [
+        { id: 1, content: '' },
+        { id: 2, content: '' }
+      ]
+    }
   },
   mounted() {
     // TODO
   },
   methods: {
-    // TODO
+    addNewQuestion() {
+      this.questions.push({
+        id: this.questions.length+1,
+        content: ''
+      })
+    }
   }
 }
 </script>
 
 <style>
-#vote-container {
+#question-container {
   flex: 1 0 0;
 
   display: flex;
@@ -38,15 +57,54 @@ export default {
   justify-content: center;
 
   /* box model */
-  /*padding: 100px 100px;*/
-  padding-top: 100px;
+  padding-top: 50px;
 
-  background-color: #fff;
+  background-color: #fafafa;
 }
 
 .inside-container {
-  background-color: #fdfdfd;
+  position: relative;
+
+  background-color: white;
+  border-radius: 20px;
+  padding: 20px;
   height: 100%;
-  width: 500px;
+  width: 80%;
+}
+
+.question-title {
+  margin-bottom: 30px;
+}
+
+.question-title .input-big {
+  width: 100%;
+}
+
+.question {
+  margin-bottom: 30px;
+}
+
+.question p {
+  margin-bottom: 5px;
+
+}
+
+.new-question {
+  height: 100px;
+  width: 100px;
+
+  border: 3px solid #ddd;
+  border-radius: 10px;
+  /*background-color: red;*/
+}
+
+.new-question:hover {
+  cursor: pointer;
+}
+
+#question-button {
+  position: absolute;
+  left: 20px;
+  bottom: 20px;
 }
 </style>
