39cba54332c2b1ff33f7a61a1092c10bb0502069
YogaPan <yogapan85321@gmail.com>
2017-09-13 15:38:50 +0800

    抄襲hangout設定

--- app/chatroom/router/Screen.vue
+++ app/chatroom/router/Screen.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="screen-container">
-    <h1 class="title">Screen share here...</h1>
     <video id="localVideo"></video>
-    <div id="remoteVideos"></div>    
+
+    <div id="remote-video-bar">
+      <div id="remoteVideos"></div>
+    </div>
+
+    
+    <div id="button-bar">
+      <div class="circle-button"></div>
+      <div id="hang-up" class="circle-button"></div>
+      <div class="circle-button"></div>
+    </div>
+
   </div>
 </template>
 
@@ -20,7 +30,8 @@ export default {
       // the id/element dom element that will hold remote videos
       remoteVideosEl: 'remoteVideos',
       // immediately ask for camera access
-      autoRequestMedia: true
+      autoRequestMedia: true,
+      url: 'http://localhost:8888'
     })
 
     webrtc.on('readyToCall', function () {
@@ -37,10 +48,72 @@ export default {
 <style>
 
 #screen-container {
+  position: relative;
   flex: 1 0 0;
+}
+
+#localVideo {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  min-width: 100%; 
+  min-height: 100%;
+  width: auto; 
+  height: auto;
+
+  overflow: hidden;
+  z-index: -1;
+}
+
+#remote-video-bar {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 150px;
+  height: 100%;
+  background-color: black;
+  opacity: 0.5; 
+}
+
+#remoteVideos {
+
+}
+
+.circle-button {
+  height: 65px;
+  width: 65px;
+
+  border-radius: 50%;
+  background-color: black;
+  opacity: 0.7;
+}
+
+#hang-up {
+  background-color: red;
+}
+
+#button-bar {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  height: 100px;
+  width: 100%;
+  padding-left: 35%;
+  padding-right: 35%;
+
+  /* background-color: black; */
+  /* opacity: 0.5; */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 
 .title {
   color: white;
+  font-size: 20px;
 }
 </style>
