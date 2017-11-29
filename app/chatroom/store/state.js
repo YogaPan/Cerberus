150fad4d140e0cb4de91f2c974bd52976c6bcc04
YogaPan <galtz0321@gmail.com>
2017-11-29 13:43:21 +0800

    修正投票socket錯誤

--- app/chatroom/store/state.js
+++ app/chatroom/store/state.js
@@ -1,16 +1,18 @@
 export const state = {
   count: 3,
   messages: [
-     // { id: '2', content: '呵呵哈哈笑三小😄 https://www.youtube.com/watch?v=fAGlaNUBMTM', time: '00:00', read: true },
-     // { id: '1', content: 'second second second', time: '00:01', read: true },
-     // { id: '0', content: 'third third third', time: '00:13', read: false },
+    // { id: '2', content: '呵呵哈哈笑三小😄 https://www.youtube.com/watch?v=fAGlaNUBMTM', time: '00:00', read: true },
+    // { id: '1', content: 'second second second', time: '00:01', read: true },
+    // { id: '0', content: 'third third third', time: '00:13', read: false },
   ],
   question: '',
-  options: [
-    // { id: 1, content: '一本道', isSelected: false },
-    // { id: 2, content: '東京熱', isSelected: false },
-    // { id: 3, content: 'JAV', isSelected: false }
-  ],
+  options: [],
+  // question: '想要去哪裡玩？',
+  // options: [
+  //   { id: 1, content: '東京', isSelected: false },
+  //   { id: 2, content: '大阪', isSelected: false },
+  //   { id: 3, content: '北海道', isSelected: false }
+  // ],
 
   webrtc: null,
   streaming: false,
